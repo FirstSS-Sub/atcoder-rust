8e7593b463d69416d560a9d9e5ef0e41a3b3a31c
FirstSS-Sub <sota.monokuma.bass.1@gmail.com>
2021-11-20 18:10:21 +0900

    template change

--- typical90/target/debug/002.d
+++
@@ -1,1 +0,0 @@
-/home/ss/rust/atcoder-rust/typical90/target/debug/002: /home/ss/rust/atcoder-rust/typical90/src/bin/002.rs
--- typical90/target/debug/deps/002-dbddd8c787d64d08.d
+++
@@ -1,5 +0,0 @@
-/home/ss/rust/atcoder-rust/typical90/target/debug/deps/002-dbddd8c787d64d08: src/bin/002.rs
-
-/home/ss/rust/atcoder-rust/typical90/target/debug/deps/002-dbddd8c787d64d08.d: src/bin/002.rs
-
-src/bin/002.rs:

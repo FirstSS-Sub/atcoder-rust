90681e11121d63015e41a5cb5faa81da862ebbe4
Sota Saito <sota.monokuma.bass.1@gmail.com>
2021-11-26 20:07:33 +0900

    typical90 014

--- typical90/src/bin/014.rs
+++ typical90/src/bin/014.rs
@@ -2,5 +2,17 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut a: [i64; n],
+        mut b: [i64; n]
+    }
+    a.sort_unstable();
+    b.sort_unstable();
+
+    let mut ans = 0;
+    for i in 0..n {
+        ans += (a[i] - b[i]).abs();
+    }
+    println!("{}", ans);
 }
---
+++ typical90/target/debug/deps/014-96b3d72974ae778a.d
@@ -0,0 +1,5 @@
+/Users/s10974/rust/atcoder-rust/typical90/target/debug/deps/014-96b3d72974ae778a: src/bin/014.rs
+
+/Users/s10974/rust/atcoder-rust/typical90/target/debug/deps/014-96b3d72974ae778a.d: src/bin/014.rs
+
+src/bin/014.rs:
